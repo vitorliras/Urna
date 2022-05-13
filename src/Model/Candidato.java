4ce3c9b2b1c5ec453d492a9dc6c9b62a006d1e5b
Vitor Lira <vitorlirasti@hotmail.com>
2022-05-13 18:03:15 -0300

    Criando e dando valor as classes candidato, partido e pessoa

--- src/Model/Candidato.java
+++ src/Model/Candidato.java
@@ -1,5 +1,22 @@
 package Model;
 
-public class Candidato {
+public class Candidato extends Pessoa{
+	private Partido partido;
+	private int quantidadeVotos;
+	
+	
+	public Partido getPartido() {
+		return partido;
+	}
+	public void setPartido(Partido partido) {
+		this.partido = partido;
+	}
+	public int getQuantidadeVotos() {
+		return quantidadeVotos;
+	}
+	public void setQuantidadeVotos(int quantidadeVotos) {
+		this.quantidadeVotos = quantidadeVotos;
+	}
+	
 	
 }
